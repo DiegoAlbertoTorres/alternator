22fdfe66e2a7dc4b7bbe02ab04ea72b44e2cc8bb
Diego Torres Quintanilla <diegoalbertotorres@gmail.com>
2015-10-23 00:25:08 -0400

    Packets are now serialized using gob

--- mail/mail.go
+++ mail/mail.go
@@ -1,23 +1,37 @@
-/* Package responsible for sending 'messages' to other nodes.
- * */
+// Package mail is responsible for sending 'Packets' to other nodes.
 package mail
 
+// Peer is an entity that a node can send packets to.
 type Peer struct {
 	Protocol string
 	Address  string
 }
 
+// Define packet types
+const (
+	Join = iota
+)
+
+// Packet is what is sent by mail
 type Packet struct {
 	Content string
+	Type    int
+
+	Peer Peer
 }
 
+func (p Packet) String() string {
+	return p.Content + " " + p.Peer.Protocol + " " + p.Peer.Address
+}
+
+// Send mails a packet
 func Send(packet *Packet, peer *Peer) {
 	if peer.Protocol == "UDP" {
 		udpSend(packet, peer.Address)
 	}
 }
 
-// Returns the receiving port
+// Receive is the API call to catch a packet in a protocol's queue
 func Receive(protocol string, ret *Packet) {
 	if protocol == "UDP" {
 		udpReceive(ret)
--- mail/udp.go
+++ mail/udp.go
@@ -1,16 +1,43 @@
-/* UDP protocol functionality implementation
+/*Package mail UDP protocol functionality implementation
  * */
 package mail
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"net"
 	"os"
-  "strings"
-  "strconv"
+	"strconv"
+	"strings"
 )
 
 var serverConn *net.UDPConn
+
+// UDPStrAddr converts an IP (as a string) and a port into a single address string
+func UDPStrAddr(ip string, port int) string {
+	return ip + ":" + strconv.Itoa(port)
+}
+
+// UDPBind must be called before using mail.receive for UDP
+func UDPBind() int {
+	var err error
+	var serverAddr *net.UDPAddr
+
+	// Port 0 means find some port
+	serverAddr, err = net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	checkError(err)
+
+	// Now listen there, this also resolves the port
+	serverConn, err = net.ListenUDP("udp", serverAddr)
+	checkError(err)
+	// For some reason, you can only get the chosen port by extracting from this string
+	// Apparently there is no parser either
+	port, err := strconv.Atoi(strings.Split(serverConn.LocalAddr().String(), ":")[1])
+	checkError(err)
+
+	return port
+}
 
 func udpSend(packet *Packet, addr string) {
 	udpAddr, err := net.ResolveUDPAddr("udp", addr)
@@ -20,46 +47,44 @@ func udpSend(packet *Packet, addr string) {
 	conn, err := net.DialUDP("udp", nil, udpAddr)
 	checkError(err)
 
-	_, err = conn.Write([]byte(packet.Content))
+	// Will use gob for serialization.
+	// _, err = conn.Write([]byte(packet.Content))
+	// buf := make([]byte, 1024)
+	// bufferedWriter := bytes.NewReader(buf)
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	// Hopefully fits in a single UDP packet
+	enc.Encode(packet)
+	conn.Write(buf.Bytes())
 	checkError(err)
 }
 
-// Must be called before using mail.receive for UDP
-func UdpBind() int {
-  var err error
-  var serverAddr *net.UDPAddr
-
-	// Resolve local ip
-	serverAddr, err = net.ResolveUDPAddr("udp", "127.0.0.1:0")
-  checkError(err)
-
-  // Now listen there, this also resolves the port
-	serverConn, err = net.ListenUDP("udp", serverAddr)
-	checkError(err)
-  // For some reason, you can only get the chosen port by extracting from this string
-  // Apparently there is no parser either
-  port, err := strconv.Atoi(strings.Split(serverConn.LocalAddr().String(), ":")[1])
-  checkError(err)
-
-  return port
-}
-
-func UdpClose() {
-  serverConn.Close()
+// UDPClose closes the UDP connection
+func UDPClose() {
+	serverConn.Close()
 }
 
 func udpReceive(ret *Packet) {
-
 	buf := make([]byte, 1024)
+	// var buf bytes.Buffer
 
-	n, _, err := serverConn.ReadFromUDP(buf)
+	_, sender, err := serverConn.ReadFromUDP(buf)
+	// fmt.Println(buf)
+	// fmt.Println([]byte("hello!"))
+	// Now unserialize with gob
+	bufferedReader := bytes.NewReader(buf)
+	dec := gob.NewDecoder(bufferedReader)
+	dec.Decode(ret)
+
 	checkError(err)
-	ret.Content = string(buf[0:n])
+	ret.Peer.Protocol = "UDP"
+	ret.Peer.Address = UDPStrAddr(sender.IP.String(), sender.Port)
+	// ret.Content = string(buf[0:n])
 }
 
 func checkError(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error ", err.Error())
+		fmt.Fprintln(os.Stderr, "Fatal error ", err.Error())
 		os.Exit(1)
 	}
 }
